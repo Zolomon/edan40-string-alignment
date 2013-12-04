58621613e0241e792ab5d49925c385a2fc09475a
zol <zolomon@gmail.com>
2013-12-04 21:00:39 +0100

    almost working

--- stringalignment.hs
+++ stringalignment.hs
@@ -12,27 +12,47 @@ If we had access to an algorithm, then we could use it to solve the MCS by setti
 
 module StringAlignment where
 
-scoreMatch    = 1
+scoreMatch    = 0
 scoreMismatch = (-1)
-scoreSpace    = (-2) 
+scoreSpace    = (-1)
+string1 = "writers"
+string2 = "vintner"
 
+
+-- http://en.wikipedia.org/wiki/Smith-Waterman_algorithm
+-- Will calculate the similarity between two string sequences using a gap-scoring scheme.
 similarityScore :: String -> String -> Int
-similarityScore top bot = sum $ zipWith scorer top bot 
-  where scorer :: Char -> Char -> Int
-        scorer  _ '-' = scoreSpace
-        scorer '-' _  = scoreSpace
-        scorer  x  y 
-          | x == y    = scoreMatch
-          | otherwise = scoreMismatch
+similarityScore xs ys = getScore (length xs) (length ys)
+  where getScore :: Int -> Int -> Int
+        getScore i j = scoreTable !! i !! j
+        
+        scoreTable :: [[Int]]
+        scoreTable = [[scoreEntry i j | j <- [0..] ] | i <- [0..]]
+        
+        scoreEntry :: Int -> Int -> Int
+        scoreEntry 0 0 = 0
+        scoreEntry i 0 = scoreSpace * i -- w(a, -), took forever to realize :(
+        scoreEntry 0 j = scoreSpace * j -- w(-, b), -"-
+        scoreEntry i j = maximum [ (getScore (i-1) (j-1)) + (scorer x   y)
+                                 , (getScore (i-1) (  j)) + (scorer x '-')
+                                 , (getScore (  i) (j-1)) + (scorer '-' y)
+                                 ]
+          where x = xs !! (i-1)
+                y = ys !! (j-1)
+
+-- Calculates the score for a column in the two sequences. 
+scorer :: Char -> Char -> Int
+scorer  _ '-' = scoreSpace
+scorer '-' _  = scoreSpace
+scorer  x  y 
+  | x == y    = scoreMatch
+  | otherwise = scoreMismatch
                         
 -- 2.b)
 -- `attachHeads` takes two arguments of type a, and conses them onto each list, in 
 -- the list of tuples of lists. That is, executing the below in GHCi:
-
--- Prelude> attachHeads 0 (-1) [([1],[2]),([3],[4])] 
--- [([0,1],[-1,2]),([0,3],[-1,4])].
-
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachHeads h1 h2 aList = [(h1:xs, h2:ys) | (xs, ys) <- aList]
 
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
 maximaBy valueFcn xs = map fst $ filter ((>= (maximum $ transformed)) . snd ) $ zip xs $ transformed
@@ -41,4 +61,25 @@ maximaBy valueFcn xs = map fst $ filter ((>= (maximum $ transformed)) . snd ) $ 
 type AlignmentType = (String, String)
 
 optAlignments :: String -> String -> [AlignmentType]
-optAlignments string1 string2 
+optAlignments xs ys  = map (\(x,y) -> (reverse x, reverse y)) $ getAlignment (length xs) (length ys)
+  where getAlignment :: Int -> Int -> [AlignmentType]
+        getAlignment i j = alignmentTable !! i !! j
+
+        alignmentTable :: [[[AlignmentType]]]
+        alignmentTable = [[alignmentEntry i j | j <- [0..]] | i <- [0..]]
+        
+        alignmentEntry :: Int -> Int -> [AlignmentType]
+        alignmentEntry 0 0 = [([], [])]
+        alignmentEntry i 0 = attachHeads (xs !! (i-1)) (        '-') $ getAlignment (i-1) 0
+        alignmentEntry 0 j = attachHeads (        '-') (ys !! (j-1)) $ getAlignment 0 (j-1)
+        alignmentEntry i j 
+          | (xs !! (i-1)) == (ys !! (j-1)) = attachHeads (xs !! (i-1)) (ys !! (j-1)) $ getAlignment (i-1) (j-1)
+          -- maximaBy similarity will reduce the generated list
+          | otherwise = maximaBy similarity $ concat  [attachHeads (xs !! (i-1)) (ys !! (j-1)) $ getAlignment (i-1) (j-1)
+                                                     ,attachHeads (        '-') (ys !! (j-1)) $ getAlignment     i (j-1)
+                                                     ,attachHeads (xs !! (i-1)) (        '-') $ getAlignment (i-1)     j
+                                                     ]
+                        
+similarity :: AlignmentType -> Int
+similarity (xs,ys) = similarityScore xs ys
+
